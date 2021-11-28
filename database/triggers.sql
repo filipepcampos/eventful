a8b3079777c977d8bea5411d5ab3120f807278e2
Filipe Campos <filipepcampos24@gmail.com>
2021-11-28 14:13:07 +0000

    Small bug fixes so triggers can run :hammer:

--- database/triggers.sql
+++ database/triggers.sql
@@ -1,10 +1,12 @@
+SET search_path TO lbaw2122;
+
 -- ===== TRIGGER01 =====
 DROP TRIGGER IF EXISTS event_attendee_dif_host ON attendee;
 DROP FUNCTION IF EXISTS event_attendee_dif_host;
 CREATE FUNCTION event_attendee_dif_host() RETURNS TRIGGER AS $$
 BEGIN
     IF EXISTS (SELECT * FROM event WHERE NEW.id_event = event.id AND NEW.id_user = event.id_host) THEN
-        RAISE EXCEPTION "The user you're trying to add to attendees is the event host"
+        RAISE EXCEPTION 'The user you''re trying to add to attendees is the event host';
     END IF;
     RETURN NEW;
 END
@@ -18,7 +20,7 @@ CREATE TRIGGER event_attendee_dif_host
 
 -- ===== TRIGGER02 =====
 DROP TRIGGER IF EXISTS event_request ON request;
-DROP FUNCTION IF EXISTS event_attendee_dif_host;
+DROP FUNCTION IF EXISTS event_request;
 CREATE FUNCTION event_request() RETURNS TRIGGER AS $$
 BEGIN
         IF EXISTS (SELECT * FROM (OLD LEFT JOIN event ON (id_event = id)) WHERE is_visible AND NOT is_acessible) THEN
@@ -37,7 +39,7 @@ CREATE TRIGGER event_request
 -- ===== TRIGGER03 =====
 DROP TRIGGER IF EXISTS user_report_trigger ON user_report;
 DROP FUNCTION IF EXISTS user_report;
-CREATE FUNCTION user_report() RETURNS TRIGGER AS
+CREATE FUNCTION user_report() RETURNS TRIGGER AS $$
 BEGIN
     IF EXISTS (SELECT * FROM (OLD LEFT JOIN report ON (user_report = id)) WHERE id_author = target) THEN
         RAISE EXCEPTION 'Users can not report themselves.';
@@ -51,3 +53,14 @@ CREATE TRIGGER user_report_trigger
     BEFORE INSERT OR UPDATE ON user_report
     FOR EACH ROW
     EXECUTE PROCEDURE user_report();
+
+-- ===== TRIGGER04 =====
+
+-- -- ===== TRIGGER05 =====
+-- DROP TRIGGER IF EXISTS event_report_trigger ON event_report;
+-- DROP FUNCTION IF EXISTS event_report_trigger;
+-- 
+-- CREATE FUNCTION event_report() RETURNS TRIGGER AS
+-- BEGIN
+--     IF EXISTS (SELECT * FROM (OLD LEFT JOIN ))
+-- END
