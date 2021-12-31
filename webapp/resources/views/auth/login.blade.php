ac5d090822baf71b472aa4d13c5d6f3b1fc4290d
Filipe Campos <filipepcampos24@gmail.com>
2021-12-31 00:01:26 +0000

    Mix is now working, yay

--- webapp/resources/views/auth/login.blade.php
+++ webapp/resources/views/auth/login.blade.php
@@ -8,23 +8,23 @@
     
     <div class="form-group">
         <label for="email">Email</label>
-        <input type="email" class="form-control" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" value="{{ old('email') }}" required autofocus>
+        <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" aria-describedby="emailHelp" placeholder="Enter email" value="{{ old('email') }}" required autofocus>
     </div>
-    @if ($errors->has('email'))
-        <span class="error">
-        {{ $errors->first('email') }}
+    @error('email')
+        <span class="invalid-feedback" role="alert">
+            <strong>{{ $message }}</strong>
         </span>
-    @endif
+    @enderror
 
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" name="password" id="password" placeholder="Enter password" value="{{ old('password') }}" required>
+        <input type="password" class="form-control  @error('password') is-invalid @enderror" name="password" id="password" placeholder="Enter password" value="{{ old('password') }}" required>
     </div>
-    @if ($errors->has('password'))
-    <span class="error">
-        {{ $errors->first('password') }}
+    @error('password')
+    <span class="invalid-feedback" role="alert">
+        <strong>{{ $message }}</strong>
     </span>
-    @endif
+    @enderror
 
     <div class="form-check">
         <input type="checkbox" class="form-check-input" name="remember" {{ old('remember') ? 'checked' : '' }}>
--- webapp/resources/views/layouts/app.blade.php
+++ webapp/resources/views/layouts/app.blade.php
@@ -13,12 +13,6 @@
     <!-- Styles -->
     <link href="{{ asset('css/milligram.min.css') }}" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    
-    <!-- Font -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet"> 
 
     <script type="text/javascript">
         // Fix for Firefox autofocus CSS bug
@@ -76,6 +70,5 @@
         @yield('content')
       </section>
     </main>
-    @include('layouts.scripts')
   </body>
 </html>
