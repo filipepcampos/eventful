d3bae4a941e73dfaa84efcd708d6492849f02fc0
Filipe Campos <filipepcampos24@gmail.com>
2022-01-20 08:48:18 +0000

    Add required asterisks

--- webapp/resources/views/auth/register.blade.php
+++ webapp/resources/views/auth/register.blade.php
@@ -13,7 +13,7 @@
     {{ csrf_field() }}
 
     <div class="form-group mb-3">
-        <label for="username" class="form-label">Username</label>
+        <label for="username" class="form-label required-label">Username</label>
         <input id="username" class="form-control @if($errors->has('username')) is-invalid @endif" type="text" name="username" value="{{ old('username') }}" placeholder="Enter username" required autofocus>
         @if ($errors->has('username'))
         <div class="invalid-feedback d-block">
@@ -23,7 +23,7 @@
     </div>
 
     <div class="form-group mb-3">
-        <label for="name" class="form-label">Name</label>
+        <label for="name" class="form-label required-label">Name</label>
         <input id="name" class="form-control @if($errors->has('name')) is-invalid @endif" type="text" name="name" value="{{ old('name') }}" placeholder="Enter name" required>
         @if ($errors->has('name'))
         <div class="invalid-feedback d-block">
@@ -33,7 +33,7 @@
     </div>
 
     <div class="form-group mb-3">
-        <label for="email" class="form-label">Email Address</label>
+        <label for="email" class="form-label required-label">Email Address</label>
         <input id="email" class="form-control @if($errors->has('email')) is-invalid @endif" type="email" name="email" value="{{ old('email') }}" placeholder="Enter email" required>  
         @if ($errors->has('email'))
         <div class="invalid-feedback d-block">
@@ -43,7 +43,7 @@
     </div>
 
     <div class="form-group mb-3">
-        <label for="birthdate" class="form-label">Birthdate</label>
+        <label for="birthdate" class="form-label required-label">Birthdate</label>
         <input id="birthdate" class="form-control @if($errors->has('birthdate')) is-invalid @endif" type="date" name="birthdate" value="{{ old('birthdate') }}" required>
         @if ($errors->has('birthdate'))
         <div class="invalid-feedback d-block">
@@ -53,7 +53,7 @@
     </div>
 
     <div class="form-group mb-3">
-        <label for="password" class="form-label">Password</label>
+        <label for="password" class="form-label required-label">Password</label>
         <input id="password" class="form-control @if($errors->has('password')) is-invalid @endif" type="password" name="password" required>
         <span class="form-text">Password must be at least 8 characters long and contain lowercase, uppercase, numerical and special characters.</span>
         @if ($errors->has('password'))
