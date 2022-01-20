fcd15104baf307017bd4ddaa6ed3efdbb41efc9d
Filipe Campos <filipepcampos24@gmail.com>
2022-01-20 22:50:52 +0000

    Remove mail TLS because lbaw2122.lbaw.fe.up.pt doesnt have a valid certificate

--- webapp/config/mail.php
+++ webapp/config/mail.php
@@ -84,8 +84,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'team@eventful.com'),
+        'name' => env('MAIL_FROM_NAME', 'Eventful'),
     ],
 
     /*
